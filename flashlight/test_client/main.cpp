4af289b88b72243c312de908584971f47f590fea
roynatech2544 <whiteshell2544@naver.com>
2023-05-23 10:22:29 +0900

    flashlight: test_client: Use libextsupport

--- flashlight/test_client/main.cpp
+++ flashlight/test_client/main.cpp
@@ -1,34 +1,25 @@
 #include <android/binder_manager.h>
 
 #include <aidl/vendor/samsung_ext/hardware/camera/flashlight/BnFlashlight.h>
+
 #include <cstdio>
 #include <unistd.h>
 
-using SpB = ndk::SpAIBinder;
+#include <GetServiceSupport.h>
+#include <TestLogSupport.h>
+
 using aidl::vendor::samsung_ext::hardware::camera::flashlight::IFlashlight;
 
-#define TEST_LOG_EXT(name, arg, exttext, ...)                                      \
-  {                                                                            \
-    auto rc = svc->name(arg);                                                  \
-    printf("%s: ok: %s" exttext "\n", #name, rc.isOk() ? "true" : "false",     \
-           ##__VA_ARGS__);                                                     \
-    sleep(1);                                                                  \
-  }
-
-#define TEST_LOG(name, arg) TEST_LOG_EXT(name, arg, "")
-
 int main(void) {
-  auto binder = AServiceManager_waitForService(
-      "vendor.samsung_ext.hardware.camera.flashlight.IFlashlight/default");
-  if (!binder) {
-    fprintf(stderr, "waitForService returned NULL\n");
+  auto svc = getService<IFlashlight>("vendor.samsung_ext.hardware.camera.flashlight.IFlashlight/default");
+  if (!svc) {
+    printf("getService returned null\n");
     return 1;
   }
-  auto svc = IFlashlight::fromBinder(SpB(binder));
   int ret = 0;
-  TEST_LOG(enableFlash, 1);
-  TEST_LOG_EXT(getCurrentBrightness, &ret, "ret: %d", ret);
-  TEST_LOG(setBrightness, 5);
-  TEST_LOG(setBrightness, 1);
-  TEST_LOG(enableFlash, 0);
+  TEST_LOG(svc, enableFlash, 1);
+  TEST_LOG_EXT(svc, getCurrentBrightness, &ret, " ret: %d", ret);
+  TEST_LOG(svc, setBrightness, 5);
+  TEST_LOG(svc, setBrightness, 1);
+  TEST_LOG(svc, enableFlash, 0);
 }
