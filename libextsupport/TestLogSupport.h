b6c567a9fae990865c8724234e74b17fa5af7003
roynatech2544 <whiteshell2544@naver.com>
2023-06-05 20:48:45 +0900

    libextsupport: Add `TEST_LOG2` varients
     - These accepts 2 svc arguments

--- libextsupport/TestLogSupport.h
+++ libextsupport/TestLogSupport.h
@@ -1,3 +1,7 @@
+#include <unistd.h>
+
+#include <cstdio>
+
 #define TEST_LOG_EXT(svc, name, arg, exttext, ...)                             \
   {                                                                            \
     auto rc = svc->name(arg);                                                  \
@@ -7,3 +11,13 @@
   }
 
 #define TEST_LOG(svc, name, arg) TEST_LOG_EXT(svc, name, arg, "")
+
+#define TEST_LOG2_EXT(svc, name, arg1, arg2, exttext, ...)                     \
+  {                                                                            \
+    auto rc = svc->name(arg1, arg2);                                           \
+    printf("%s: ok: %s" exttext "\n", #name, rc.isOk() ? "true" : "false",     \
+           ##__VA_ARGS__);                                                     \
+    sleep(1);                                                                  \
+  }
+#define TEST_LOG2(svc, name, arg1, arg2)                                       \
+  TEST_LOG2_EXT(svc, name, arg1, arg2, "")
