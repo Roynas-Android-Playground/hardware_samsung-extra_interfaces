1a86a26e2aca0e01a603c368633904b43f019d11
roynatech2544 <whiteshell2544@naver.com>
2023-06-07 11:25:58 +0900

    libextsupport: Use checkService to bail out fast

--- libextsupport/GetServiceSupport.h
+++ libextsupport/GetServiceSupport.h
@@ -6,7 +6,7 @@
 template <typename T, std::enable_if_t<std::is_base_of_v<ndk::ICInterface, T>, bool> = true>
 static inline std::shared_ptr<T> getService(const char* name)
 {
-	auto binder = AServiceManager_waitForService(name);
+	auto binder = AServiceManager_checkService(name);
 	if (!binder) return nullptr;
 	return T::fromBinder(ndk::SpAIBinder(binder));
 }
