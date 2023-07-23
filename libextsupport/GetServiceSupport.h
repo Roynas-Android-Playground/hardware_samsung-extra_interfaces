4c903b507a0e367d05155df96d30bf864c5f5859
roynatech2544 <whiteshell2544@naver.com>
2023-07-23 13:25:16 +0900

    libextsupport: Use std::string instead

--- libextsupport/GetServiceSupport.h
+++ libextsupport/GetServiceSupport.h
@@ -1,12 +1,13 @@
 #include <type_traits>
+#include <string>
 
 #include <android/binder_interface_utils.h>
 #include <android/binder_manager.h>
 
 template <typename T, std::enable_if_t<std::is_base_of_v<ndk::ICInterface, T>, bool> = true>
-static inline std::shared_ptr<T> getService(const char* name)
+static inline std::shared_ptr<T> getService(const std::string& name)
 {
-	auto binder = AServiceManager_checkService(name);
+	auto binder = AServiceManager_checkService(name.c_str());
 	if (!binder) return nullptr;
 	return T::fromBinder(ndk::SpAIBinder(binder));
 }
