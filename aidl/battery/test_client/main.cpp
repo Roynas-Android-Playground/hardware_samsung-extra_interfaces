0ce924d0e744ae569f9e394c3e51f016d249cf3d
roynatech2544 <whiteshell2544@naver.com>
2023-07-25 08:30:21 +0900

    aidl: battery_test_cli: Change to `stoi_safe`

--- aidl/battery/test_client/main.cpp
+++ aidl/battery/test_client/main.cpp
@@ -16,15 +16,18 @@ int main(int argc, const char **argv) {
     fprintf(stderr, "getService returned null\n");
     return 1;
   }
+  std::optional<int> _arg1, _arg2, _arg3;
   int arg1, arg2, arg3;
-  try {
-    arg1 = std::stoi(argv[1]);
-    arg2 = std::stoi(argv[2]);
-    arg3 = std::stoi(argv[3]);
-  } catch (const std::exception &e) {
-    fprintf(stderr, "Failed to parse args: %s\n", e.what());
+  _arg1 = stoi_safe(argv[1]);
+  _arg2 = stoi_safe(argv[2]);
+  _arg3 = stoi_safe(argv[3]);
+  if (!_arg1 || !_arg2 || !_arg3) {
+    fprintf(stderr, "Failed to parse arguments to int\n");
     return 1;
   }
+  arg1 = *_arg1;
+  arg2 = *_arg2;
+  arg3 = *_arg3;
   switch (arg1) {
   case 1: {
     TEST_LOG2(svc, setChargeLimit, arg2, arg3);
