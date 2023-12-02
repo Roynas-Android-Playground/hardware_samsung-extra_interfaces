3e7c4047c436df459bd8358574430661801a092b
roynatech2544 <whiteshell2544@naver.com>
2023-12-02 15:27:58 +0900

    sepolicy: Provide a SEPolicy.mk including all definitions

---
+++ sepolicy/SEPolicy.mk
@@ -0,0 +1,9 @@
+# Include on BoardConfig.mk
+# samsung_ext service definitions
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+     hardware/samsung-ext/interfaces/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+     hardware/samsung-ext/interfaces/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+     hardware/samsung-ext/interfaces/sepolicy/public
