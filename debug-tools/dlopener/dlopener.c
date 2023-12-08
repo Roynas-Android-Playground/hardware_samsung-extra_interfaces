917f0419ef8ae7e3f8c28d956a1c259aac92b5b5
roynatech2544 <whiteshell2544@naver.com>
2023-12-09 01:10:41 +0900

    debug-tools: dlopener: Check if the argument is directory

--- debug-tools/dlopener/dlopener.c
+++ debug-tools/dlopener/dlopener.c
@@ -1,11 +1,14 @@
 #include <dlfcn.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   const char *path;
   void *handle = NULL;
-  int ret = EXIT_FAILURE;
+  int fd, ret = EXIT_FAILURE;
 
   if (argc <= 1) {
     printf("Please specify a module to load!\n");
@@ -13,6 +16,16 @@ int main(int argc, char *argv[]) {
   }
 
   path = argv[1];
+
+  // man7: EISDIR when pathname refers to a directory and the access requested
+  // involved writing (that is, O_WRONLY or O_RDWR is set).
+  fd = open(path, O_WRONLY);
+  if (fd < 0 && errno == EISDIR) {
+    printf("load: %s: is a directory\n", path);
+    return ret;
+  } else {
+    close(fd);
+  }
 
   handle = dlopen(path, RTLD_NOW);
   if (handle == NULL) {
