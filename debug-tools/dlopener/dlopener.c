bda81b4ba518c653df6ac6f3d3e7132729d6e9cc
Soo Hwan Na <whiteshell2544@naver.com>
2023-12-21 14:12:07 +0900

    debug-tools: dlopener: Harden the lstat result check
    - Also clean up logging

--- debug-tools/dlopener/dlopener.c
+++ debug-tools/dlopener/dlopener.c
@@ -2,17 +2,22 @@
 #include <errno.h>
 #include <limits.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define DLOPENER_PRINTF_EARLY(fmt, ...) printf("%s: " fmt "\n", argv[0], ##__VA_ARGS__)
-#define DLOPENER_PRINTF(fmt, ...) printf("%s: load %s: " fmt "\n", argv[0], path, ##__VA_ARGS__)
-#define DLOPENER_PERROR(operation) printf("%s: load %s: %s: %s\n", argv[0], path, operation, strerror(errno))
+#define DLOPENER_PRINTF_EARLY(fmt, ...)                                        \
+  printf("%s: " fmt "\n", argv[0], ##__VA_ARGS__)
+#define DLOPENER_PRINTF(fmt, ...)                                              \
+  printf("%s: load %s: " fmt "\n", argv[0], path, ##__VA_ARGS__)
+#define DLOPENER_PERROR(operation)                                             \
+  printf("%s: load %s: %s: %s\n", argv[0], path, operation, strerror(errno))
 
 int main(int argc, char *argv[]) {
-  int ret = EXIT_FAILURE, free_pathbuf = 0;
+  int ret = EXIT_FAILURE;
+  bool free_pathbuf = false;
   char *path = NULL;
   void *handle = NULL;
   struct stat buf;
@@ -25,20 +30,25 @@ int main(int argc, char *argv[]) {
   path = argv[1];
 
   ret = lstat(path, &buf);
-  if (ret < 0) {
-    DLOPENER_PERROR("stat");
-    DLOPENER_PRINTF("Will use system library path");
-  } else if (S_ISLNK(buf.st_mode)) {
-    DLOPENER_PRINTF("Following symlink");
-    path = realpath(path, NULL);
-    if (!path) {
-      DLOPENER_PERROR("realpath");
-      return ret;
+  if (ret == 0) {
+    if (S_ISLNK(buf.st_mode)) {
+      DLOPENER_PRINTF("Following symlink");
+      path = realpath(path, NULL);
+      if (!path) {
+        DLOPENER_PERROR("realpath");
+        goto out;
+      }
+      free_pathbuf = true;
+      ret = lstat(path, &buf);
+      if (ret) {
+        DLOPENER_PERROR("lstat");
+        goto out_free;
+      }
     }
-    free_pathbuf = 1;
-  } else if (!S_ISREG(buf.st_mode)) {
-    DLOPENER_PRINTF("load %s: Not a regular file", path);
-    return ret;
+    if (!S_ISREG(buf.st_mode)) {
+      DLOPENER_PRINTF("Not a regular file");
+      goto out_free;
+    }
   }
 
   handle = dlopen(path, RTLD_NOW);
@@ -50,7 +60,10 @@ int main(int argc, char *argv[]) {
     DLOPENER_PRINTF("Succeeded");
     ret = EXIT_SUCCESS;
   }
+
+out_free:
   if (free_pathbuf)
     free(path);
+out:
   return ret;
 }
